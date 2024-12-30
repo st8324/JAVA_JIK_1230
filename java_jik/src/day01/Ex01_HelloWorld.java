df8e12337da8c87e8472aa07a9e01b46422ef7a9
st8324 <stajun@naver.com>
2024-12-30 12:38:29 +0900

    자바 01일차 : Hello World 예제

---
+++ java_jik/src/day01/Ex01_HelloWorld.java
@@ -0,0 +1,18 @@
+package day01;
+
+// 한줄 주석. //가 나온 뒤부터 주석
+/* 여러줄 주석. 이 사이에 있는 모든 글자들이 주석입니다. */
+public class Ex01_HelloWorld {
+	/**
+	 * 메소드를 설명할 때 사용하는 주석입니다.
+	 * @param args 뭔지는 모릅니다.
+	 */
+	//프로그램이 실행되려면 main이 있어야 한다.
+	public static void main(String[] args) {
+		// 들여쓰기는 Tab
+		// 내어쓰기는 Shift + Tab
+		System.out.println("Hello World!");//Hello World!를 출력합니다.
+		
+	}
+
+}
