225ccf0a5f782af324dd9bcf769b3421092c9561
st8324 <stajun@naver.com>
2024-12-30 17:30:08 +0900

    자바 01일차 : 변수 연습 예제

---
+++ java_jik/src/day01/Ex09_VariableTest.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class Ex09_VariableTest {
+
+	public static void main(String[] args) {
+		
+		/* 학생의 국어, 영어, 수학 성적을 저장하기 위한 변수를 선언하세요. */
+		int korScore, engScore, mathScore;
+		
+		/* 학생의 국어, 영어, 수학 성적의 평균을 저장하기 위한 변수를 선언하세요. */
+		double average;
+		
+		/* 학생의 학점을 저장하기 위한 변수를 선언하세요. A, B, C, D, F만 저장 */
+		char result;
+		String result2;
+	}
+
+}
