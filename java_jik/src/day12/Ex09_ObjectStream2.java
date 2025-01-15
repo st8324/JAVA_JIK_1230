e428bf74d07bc73c86089fd3e2a04f211ff517b8
st8324 <stajun@naver.com>
2025-01-15 16:49:49 +0900

    자바 12일차 : IO 스트림 예제 - 객체 기반

---
+++ java_jik/src/day12/Ex09_ObjectStream2.java
@@ -0,0 +1,28 @@
+package day12;
+
+public class Ex09_ObjectStream2 {
+
+	public static void main(String[] args) {
+		/* 다음 기능을 갖는 프로그램을 작성하세요.
+		 * 단, 저장기능과 불러오기 기능을 추가
+		 * - 저장은 프로그램 종료하기 전
+		 * - 불러오기는 프로그램 시작 전
+		 * 
+		 * 1. 자동차 추가
+		 * 2. 자동차 조회(전체)
+		 * 3. 종료
+		 * 
+		 * 힌트
+		 * ArrayList 클래스도 Serializable 인터페이스를 구현한 구현 클래스이다.
+		 * */
+		
+
+	}
+
+}
+
+class Car{
+	
+	private String name;
+	private String brand;
+}
