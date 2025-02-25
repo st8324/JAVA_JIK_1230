f9b2c5ceb9d6cac102368eb4acd562d24b6a9e95
st8324 <stajun@naver.com>
2025-02-25 12:50:33 +0900

    DB 01일차 : 기본 SQL

---
+++ db/01_기본 SQL.sql
@@ -0,0 +1,19 @@
+# 주석 
+-- 주석
+/* 여러줄 주석 */
+
+# 데이터베이스 목록 확인
+SHOW DATABASES;
+
+# 데이터베이스 선택 # 워크벤츠에서 데이터베이스를 더블 클릭 => 두꺼운 글자색으로 변경 
+# USE DB명
+USE WORLD;
+
+# 선택된 데이터베이스에 있는 테이블 목록 확인
+SHOW TABLES;
+
+# 테이블 정보 확인
+# EXPLAIN 테이블명
+# DESC 테이블명
+EXPLAIN CITY;
+DESC CITY;
