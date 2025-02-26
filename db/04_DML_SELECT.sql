515a797103e927f4d222e7da772d9bc849ad79d4
st8324 <stajun@naver.com>
2025-02-26 17:09:42 +0900

    DB 02일차 : DML - SELECT, DDL

--- db/04_DML_SELECT.sql
+++ db/04_DML_SELECT.sql
@@ -192,7 +192,8 @@ DROP VIEW STUDENT_SCORE;
 CREATE VIEW STUDENT_SCORE AS 
 	SELECT 
 		ST_GRADE 학년, ST_CLASS 반, ST_NUM 번호, ST_NAME 이름,
-		SJ_GRADE 과목학년, SJ_SEMESTER 학기, SJ_NAME 과목명, SC_SCORE 성적
+		SJ_GRADE 과목학년, SJ_SEMESTER 학기, SJ_NAME 과목명, SC_SCORE 성적,
+        ST_KEY 학생번호
 	FROM
 		SCORE
 	JOIN
@@ -201,8 +202,161 @@ CREATE VIEW STUDENT_SCORE AS
 		SUBJECT ON SC_SJ_NUM = SJ_NUM;
 
 # 1학년의 1학년 1학기 국어 성적을 조회하는 쿼리 
-SELECT * FROM STUDENT_SCORE;
+SELECT * FROM STUDENT_SCORE WHERE 학년 = 1 AND 과목학년 = 1 AND 학기 = 1 AND 과목명 = "국어";
 
 # 1학년의 1학년 1학기 국어 성적의 평균을 조회하는 쿼리 
+SELECT 학년, 과목학년, 학기, 과목명, AVG(성적) 평균 FROM STUDENT_SCORE WHERE 학년 = 1 AND 과목학년 = 1 AND 학기 = 1 AND 과목명 = "국어";
 
-# 1학년의 1학년 1학기 국어 성적의 평균을 각 반별로 조회하는 쿼리 
+# 2학년의 1학년 2학기 국어 성적의 평균을 각 반별로 조회하는 쿼리 
+SELECT 
+	학년, 반, 과목학년, 학기, 과목명, AVG(성적) 평균 
+FROM 
+	STUDENT_SCORE 
+WHERE 
+	학년 = 2 AND 과목학년 = 1 AND 학기 = 2 AND 과목명 = "국어"
+GROUP BY 반;
+
+# 각 학생별 평균(학년, 학기별)을 조회 하는 쿼리
+SELECT 학년, 반, 번호, 이름, 과목학년, 학기, AVG(성적) 평균 FROM STUDENT_SCORE 
+GROUP BY 학생번호, 과목학년, 학기;
+
+# 각 학생의 학년별 평균을 조회하는 쿼리
+SELECT 학년, 반, 번호, 이름, 과목학년, AVG(성적) 평균 FROM STUDENT_SCORE 
+GROUP BY 학생번호, 과목학년;
+
+# 각 학생의 1학년 평균이 가장 높은 학생을 조회하는 쿼리 
+SELECT 학년, 반, 번호, 이름, 과목학년, AVG(성적) 평균 FROM STUDENT_SCORE 
+WHERE 과목학년 = 1
+GROUP BY 학생번호, 과목학년
+ORDER BY 평균 DESC
+LIMIT 1;
+
+/*
+OUTER 조인 
+SELECT 속성, ... 
+FROM 테이블A 
+LEFT | RIGHT JOIN 테이블 B ON 테이블A.속성1 = 테이블B.속성2 
+  - OUTER 조인은 순서가 중요 
+  - 왼쪽 테이블을 기준으로 연결 
+*/
+# 모든 과목의 평균을 조회하는 쿼리 
+SELECT SUBJECT.*, IFNULL(AVG(SC_SCORE), 0) 평균
+FROM 
+	SUBJECT
+LEFT JOIN
+	SCORE ON SJ_NUM = SC_SJ_NUM
+GROUP BY SJ_NUM;
+SELECT SUBJECT.*, IFNULL(AVG(SC_SCORE), 0) 평균
+FROM 
+	SCORE
+RIGHT JOIN
+	SUBJECT ON SJ_NUM = SC_SJ_NUM
+GROUP BY SJ_NUM;
+
+# 모든 학생의  학생 평균을 조회하는 쿼리. 성적이 없는 경우는 0이 나오도록 처리. 
+SELECT STUDENT.*, IFNULL(AVG(SC_SCORE), 0) 평균
+FROM STUDENT
+LEFT JOIN SCORE ON SC_ST_KEY = ST_KEY
+GROUP BY ST_KEY;
+
+# 등록된 학생수를 조회하는 쿼리 
+SELECT COUNT(*) 전체학생수 FROM STUDENT;
+
+# 크로스 조인 : 모든 경우의 수를 조합하여 합치는 조인 
+SELECT * FROM STUDENT JOIN SUBJECT;
+
+# JOIN USING : 테이블B의 외래키와 테이블A의 외래키가 같은 경우(속성이름이) 
+/*
+SELECT * FROM 테이블A JOIN 테이블B ON 테이블B.속성1 = 테이블A.속성1;
+SELECT * FROM 테이블A JOIN 테이블B USING(속성1);
+*/
+
+# 서브 쿼리 : 쿼리 안에 쿼리가 들어가는 형태 
+# 장점 
+# 1. 쿼리의 구조화 
+# 2. JOIN보다 가독성이 좋음 
+# 3. 속도를 향상 시킬 수 있음 
+#   홍길동이라는 사람이 KH 학원에 다니는 아들을 찾으러 왔음 
+#   일반 JOIN : 모든 강의장에 있는 학생들 중에 성이 홍씨인 남자를 찾고, 그중에 아들을 찾음 
+#   서브쿼리 : 각 강의장에서 성이 홍씨고 남자인 학생들을 찾고, 그중에서 아들을 찾음 
+# 서브쿼리 적용 위치
+# 1. SELECT문에서 속성 위치 
+# 2. SELECT문에서 FROM 다음에 나오는 테이블명 위치 
+# 3. SELECT문에서 WHERE절에서 특정 값 위치 
+# 4. SELECT문에서 HAVING절에서 특정 값 위치 
+# 5. INSERT SELECT
+# 6. UPDATE문에서 수정할 값 위치 
+
+# 내장함수 - 조건 
+# IF(식, 식1, 식2) : 식이 참이면 식1을, 거짓을 식2를 반환 
+# IFNULL(식1, 식2) : 식1이 NULL이면 식2를, 아니면 식1을 반환 
+# NULLIF(식1, 식2) : 식1과 식2가 같으면 NULL, 다르면 식1을 반환
+# CASE 속성 WHEN 값 THEN 결과 ELSE 결과 END
+# CASE 속성
+# 	WHEN 값1	THEN 
+# 		결과1 
+# 	WHEN 값2	THEN 
+# 		결과2 
+#	ELSE
+#		값3
+# END 
+
+# 성적이 60이상이면 O, 60미만이면 X라고 출력하는 쿼리 
+SELECT *, IF(SC_SCORE >= 60, "O", "X") AS 통과 FROM SCORE;
+
+# 성적이 60이상이면 O, 60미만이면 X라고 출력하는 쿼리 
+SELECT *, CASE WHEN SC_SCORE >= 60 THEN "O" ELSE "X" END AS 통과 FROM SCORE;
+
+# 성적이 90이상이면 A, 80이상이면 B, 70이상이면 C, 60이상이면 D, 60미만이면 F로 출력하는 쿼리 
+SELECT *, 
+	IF(SC_SCORE >= 90, "A", 
+		IF(SC_SCORE >= 80, "B",
+			IF(SC_SCORE >= 70, "C",
+				IF(SC_SCORE >= 60, "D", "F")))) AS 학점 FROM SCORE;
+
+SELECT *,
+	CASE 
+		WHEN SC_SCORE >= 90 THEN "A" 
+        WHEN SC_SCORE >= 80 THEN "B" 
+        WHEN SC_SCORE >= 70 THEN "C" 
+        WHEN SC_SCORE >= 60 THEN "D" 
+		ELSE "F"
+	END AS 학점
+FROM SCORE;
+
+# 성적이 최고성적과 같으면 NULL, 다르면 성적을 출력하는 쿼리
+SELECT *, NULLIF(SC_SCORE, (SELECT MAX(SC_SCORE) FROM SCORE)) AS 결과 FROM SCORE;
+
+# 내장함수 - 문자열 
+# CHAR_LENGTH(문자열) : 문자열 개수 
+SELECT CHAR_LENGTH("안녕하세요") AS CHAR_LENGTH;
+# LENGTH(문자열) : 바이트 수 
+SELECT LENGTH("안녕하세요") AS LENGTH;
+# CONCAT(문자열1, ...) : 문자열을 이어 붙임 
+SELECT CONCAT("안녕", "하", "세요?") AS CONCAT;
+# FIELD(찾을문자열, 문자열1, ....) : 찾을 문자열의 위치를 찾아 반환 . 위치는 1부터 시작
+SELECT FIELD("안녕", "안녕하", "누구 안녕?", "안녕") AS FIELD;
+# INSTR(기준문자열, 부분문자열) : 기준 문자열에서 부분 문자열의 위치를 찾아 반환. 위치는 1부터 시작
+SELECT INSTR("HELLO JAVA", "JAVA") AS INSTR;
+# LOCATE(부분문자열, 기준문자열) : 기준 문자열에서 부분 문자열의 위치를 찾아 반환 . 위치는 1부터 시작
+SELECT LOCATE("JAVA", "HELLO JAVA") AS LOCATE;
+# FORMAT(숫자, 소수점자리) : 숫자를 소수점이하 자리까지 표현. 1000단위마다 ,를 추가 
+SELECT FORMAT(10000.123456, 0) AS FORMAT;
+# BIN(숫자), OCT(숫자), HEX(숫자) : 2,8,16진수로 변환 
+SELECT BIN(255) AS BIN,OCT(255) AS OCT,HEX(255) AS HEX ;
+# INSERT(기준문자열, 위치, 길이, 삽입할문자열) : 기준문자열의 위치부터 길이만큼 지우고 삽입할 문자열을 끼워서 반환
+SELECT INSERT("HELLO JAVA", 7, 4, "C++") AS `INSERT`;
+# LEFT(문자열, 길이), RIGHT(문자열, 길이) : 왼쪽/오른족에서 문자열의 길이만큼 반환 
+SELECT LEFT("TEST.TXT", 4) AS `LEFT`, RIGHT("TEST.TXT", 3) AS `RIGHT`;
+# LOWER(문자열), UPPER(문자열) : 소문자로/대문자로 
+SELECT LOWER("HELLO JAVA") AS `LOWER`, UPPER("hello java") AS `UPPER`;
+# LPAD(문자열, 길이, 채울문자열)/RPAD(문자열,길이,채울문자열) : 문자열을 길이만큼 늘리고 빈곳을 채울문자열로 채움 
+SELECT LPAD(1, 3, "0") AS LPAD, RPAD(1, 3, "0") AS RPAD;
+# REPEAT(문자열, 횟수) : 문자열을 횟수만큼 반복 
+SELECT REPEAT(1, 3) `REPEAT`;
+# REPLACE(문자열, 문자열A, 문자열B) : 문자열에서 문자열A를 문자열B로 바꿈 
+SELECT REPLACE("HELLO JAVA", "JAVA", "C++") AS `REPLACE`;
+# REVERSE(문자열) : 문자열 순서를 역순으로 반환 
+SELECT REVERSE("ABCDEF") AS `REVERSE`;
+# SUBSTRING(문자열, 시작위치, 길이) 문자열에서 시작위치부터 길이만큼 부분문자열을 반환 
+SELECT SUBSTRING("HELLO JAVA", 7, 4) AS SUBSTRING;
