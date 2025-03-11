55c0bed3bafbac09e6990ec6f3d2889a55fb3c06
st8324 <stajun@naver.com>
2025-03-11 14:37:38 +0900

    DB 09일차 : 대학생 - DML

--- db/23_대학생_DML.sql
+++ db/23_대학생_DML.sql
@@ -148,31 +148,62 @@ WHERE
 
 # 과목 분류를 등록하는 쿼리 
 # 학과코드 등록
-
+INSERT INTO SUBJECT_CATEGORY SELECT DE_NUM FROM DEPARTMENT;
 # 교직(TEA), 교양(CUL), 기본(MSC) 등록
+INSERT INTO SUBJECT_CATEGORY VALUES("TEA"), ("CUL"), ("MSC");
 
 # 과목을 등록하는 쿼리. 
 # 과목명 : 컴퓨터 개론, 학점 : 2, 시간 : 2, 분류 : COM, 과목코드 : COM001
 # 과목명 : 프로그래밍 언어, 학점 : 3, 시간 : 3, 분류 : COM, 과목코드 : COM002
 # 과목명 : 알고리즘, 학점 : 3, 시간 : 3, 분류 : COM, 과목코드 : COM003
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "컴퓨터 개론", 2, 2, CONCAT("COM", LPAD(COUNT(*) + 1, 3, "0")), "COM"  FROM SUBJECT WHERE SJ_SC_CODE = "COM";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "프로그래밍 언어", 3, 3, CONCAT("COM", LPAD(COUNT(*) + 1, 3, "0")), "COM"  FROM SUBJECT WHERE SJ_SC_CODE = "COM";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "알고리즘", 3, 3, CONCAT("COM", LPAD(COUNT(*) + 1, 3, "0")), "COM"  FROM SUBJECT WHERE SJ_SC_CODE = "COM";
 
 # 과목명 : 확률과 통계, 학점 : 3, 시간 : 3, 분류 : MSC, 과목코드 : MSC001
 # 과목명 : 미분과 적분, 학점 : 4, 시간 : 4, 분류 : MSC, 과목코드 : MSC002
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "확률과 통계", 3, 3, CONCAT("MSC", LPAD(COUNT(*) + 1, 3, "0")), "MSC"  FROM SUBJECT WHERE SJ_SC_CODE = "MSC";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "미분과 적분", 4, 4, CONCAT("MSC", LPAD(COUNT(*) + 1, 3, "0")), "MSC"  FROM SUBJECT WHERE SJ_SC_CODE = "MSC";
+
 
 # 과목명 : 영어1, 학점 : 3, 시간 : 3, 분류 : CUL, 과목코드 : CUL001
 # 과목명 : 음악과 감상, 학점 : 2, 시간 : 2, 분류 : CUL, 과목코드 : CUL002
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "영어1", 3, 3, CONCAT("CUL", LPAD(COUNT(*) + 1, 3, "0")), "CUL"  FROM SUBJECT WHERE SJ_SC_CODE = "CUL";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "음악과 감상", 2, 2, CONCAT("CUL", LPAD(COUNT(*) + 1, 3, "0")), "CUL"  FROM SUBJECT WHERE SJ_SC_CODE = "CUL";
 
 # 과목명 : 교육학 개론, 학점 : 2, 시간 : 3, 분류 : TEA, 과목코드 : TEA001
 # 과목명 : 교육 방법론, 학점 : 3, 시간 : 3, 분류 : TEA, 과목코드 : TEA002
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "교육학 개론", 2, 3, CONCAT("TEA", LPAD(COUNT(*) + 1, 3, "0")), "TEA"  FROM SUBJECT WHERE SJ_SC_CODE = "TEA";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "교육 방법론", 3, 3, CONCAT("TEA", LPAD(COUNT(*) + 1, 3, "0")), "TEA"  FROM SUBJECT WHERE SJ_SC_CODE = "TEA";
 
 # 과목명 : 디자인의 이해, 학점 : 2, 시간 : 2, 분류 : DEG, 과목코드 : DEG001
 # 과목명 : 시각 디자인, 학점 : 3, 시간 : 3, 분류 : DEG, 과목코드 : DEG002
 # 과목명 : 제품 디자인, 학점 : 3, 시간 : 3, 분류 : DEG, 과목코드 : DEG003
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "디자인의 이해", 2, 2, CONCAT("DEG", LPAD(COUNT(*) + 1, 3, "0")), "DEG"  FROM SUBJECT WHERE SJ_SC_CODE = "DEG";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "시각 디자인", 3, 3, CONCAT("DEG", LPAD(COUNT(*) + 1, 3, "0")), "DEG"  FROM SUBJECT WHERE SJ_SC_CODE = "DEG";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "제품 디자인", 3, 3, CONCAT("DEG", LPAD(COUNT(*) + 1, 3, "0")), "DEG"  FROM SUBJECT WHERE SJ_SC_CODE = "DEG";
 
 # 과목명 : 프로그래밍 언어, 학점 : 3, 시간 : 3, 분류 : MEC, 과목코드 : MEC001
 # 과목명 : 동역학, 학점 : 3, 시간 : 3, 분류 : MEC, 과목코드 : MEC002
 # 과목명 : 기계의 이해, 학점 : 2, 시간 : 2, 분류 : MEC, 과목코드 : MEC003
-
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "프로그래밍 언어", 3, 3, CONCAT("MEC", LPAD(COUNT(*) + 1, 3, "0")), "MEC"  FROM SUBJECT WHERE SJ_SC_CODE = "MEC";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "동역학", 3, 3, CONCAT("MEC", LPAD(COUNT(*) + 1, 3, "0")), "MEC"  FROM SUBJECT WHERE SJ_SC_CODE = "MEC";
+INSERT INTO SUBJECT(SJ_TITLE, SJ_POINT, SJ_TIME, SJ_CODE, SJ_SC_CODE)
+SELECT "기계의 이해", 2, 2, CONCAT("MEC", LPAD(COUNT(*) + 1, 3, "0")), "MEC"  FROM SUBJECT WHERE SJ_SC_CODE = "MEC";
 
 
 
