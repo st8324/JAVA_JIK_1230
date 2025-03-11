5695658f4ff88bffbd4db7bff229c744ca739c5c
st8324 <stajun@naver.com>
2025-03-11 12:44:11 +0900

    DB 09일차 : 대학생 - DDL, DML

---
+++ db/23_대학생_DML.sql
@@ -0,0 +1,150 @@
+# 컴퓨터공학과를 등록. 학과코드 COM, 학과 코드 번호 160, 사무실 KH 1관 401호를 등록하는 쿼리 
+INSERT INTO DEPARTMENT VALUE("COM", "컴퓨터공학과", "사무실 KH 1관 401호", 160, NULL);
+# 디자인공학과를 등록. 학과코드 DEG, 학과 코드 번호 123, 사무실 KH 3관 101호를 등록하는 쿼리 
+INSERT INTO DEPARTMENT VALUE("DEG", "디자인공학과", "사무실 KH 3관 101호", 123, NULL);
+# 기계공학과를 등록. 학과코드 MEC, 학과 코드 번호 456, 사무실 KH 4관 201호를 등록하는 쿼리 
+INSERT INTO DEPARTMENT VALUE("MEC", "기계공학과", "사무실 KH 4관 201호", 456, NULL);
+
+#교수를 등록. 이름 홍교수, 연락처 012-1234-5678, 주민번호 601111-1, 취임연도 2000. 컴퓨터공학과
+INSERT INTO PROFESSOR 
+SELECT 
+	CONCAT("P", 2000, DE_NAME_NUM, LPAD(COUNT(PR_NUM) + 1, 3, "0")), 
+	"홍교수", "012-1234-5678", "601111-1", 2000, DE_NUM
+FROM 
+	(SELECT * FROM PROFESSOR WHERE PR_YEAR = 2000) P
+RIGHT JOIN 
+	DEPARTMENT ON DE_NUM = PR_DE_NUM
+WHERE 
+	DE_NAME = "컴퓨터공학과";
+#교수를 등록. 이름 김교수, 연락처 012-1111-5555, 주민번호 630101-2, 취임연도 2000. 컴퓨터공학과
+INSERT INTO PROFESSOR 
+SELECT 
+	CONCAT("P", 2000, 160, LPAD(COUNT(PR_NUM) + 1, 3, "0")), 
+	"김교수", "012-1111-5555", "630101-2", 2000, "COM"
+FROM 
+	PROFESSOR
+WHERE 
+	PR_YEAR = 2000;
+#교수를 등록. 이름 박교수, 연락처 012-7788-4455, 주민번호 670331-1, 취임연도 2005. 디자인공학과 
+INSERT INTO PROFESSOR 
+SELECT 
+	CONCAT("P", 2005, DE_NAME_NUM, LPAD(COUNT(PR_NUM) + 1, 3, "0")), 
+	"박교수", "012-7788-4455", "670331-1", 2005, DE_NUM
+FROM 
+	(SELECT * FROM PROFESSOR WHERE PR_YEAR = 2005) P
+RIGHT JOIN 
+	DEPARTMENT ON DE_NUM = PR_DE_NUM
+WHERE 
+	DE_NAME = "디자인공학과";
+#교수를 등록. 이름 이교수, 연락처 012-7781-4451, 주민번호 701231-2, 취임연도 2010. 디자인공학과 
+INSERT INTO PROFESSOR 
+SELECT 
+	CONCAT("P", 2010, DE_NAME_NUM, LPAD(COUNT(PR_NUM) + 1, 3, "0")), 
+	"이교수", "012-7781-4451", "701231-2", 2010, DE_NUM
+FROM 
+	(SELECT * FROM PROFESSOR WHERE PR_YEAR = 2010) P
+RIGHT JOIN 
+	DEPARTMENT ON DE_NUM = PR_DE_NUM
+WHERE 
+	DE_NAME = "디자인공학과";
+#교수를 등록. 이름 최교수, 연락처 012-3333-4444, 주민번호 650101-1, 취임연도 2005. 기계공학과 
+INSERT INTO PROFESSOR 
+SELECT 
+	CONCAT("P", 2005, DE_NAME_NUM, LPAD(COUNT(PR_NUM) + 1, 3, "0")), 
+	"최교수", "012-3333-4444", "650101-1", 2005, DE_NUM
+FROM 
+	(SELECT * FROM PROFESSOR WHERE PR_YEAR = 2005) P
+RIGHT JOIN 
+	DEPARTMENT ON DE_NUM = PR_DE_NUM
+WHERE 
+	DE_NAME = "기계공학과";
+#교수를 등록. 이름 최교수, 연락처 012-4567-4444, 주민번호 750101-1, 취임연도 2020. 기계공학과 
+INSERT INTO PROFESSOR 
+SELECT 
+	CONCAT("P", 2020, DE_NAME_NUM, LPAD(COUNT(PR_NUM) + 1, 3, "0")), 
+	"최교수", "012-4567-4444", "750101-1", 2020, DE_NUM
+FROM 
+	(SELECT * FROM PROFESSOR WHERE PR_YEAR = 2020) P
+RIGHT JOIN 
+	DEPARTMENT ON DE_NUM = PR_DE_NUM
+WHERE 
+	DE_NAME = "기계공학과";
+    
+# 최종 합격 명단
+# 이름 홍길동, 연락처 013-1234-5678, 입학연도 2025, 주민번호 060303-3, 컴퓨터공학과
+# 이름 고길동, 연락처 013-1111-1111, 입학연도 2025, 주민번호 060401-3, 컴퓨터공학과
+# 이름 김길동, 연락처 013-1111-2222, 입학연도 2025, 주민번호 060505-3, 컴퓨터공학과
+# 이름 하니, 연락처 013-1111-3333, 입학연도 2025, 주민번호 060606-4, 컴퓨터공학과
+INSERT INTO FINAL_PASS(FP_NAME, FP_PHONE, FP_YEAR, FP_RES_NUM, FP_DE_NUM) VALUES
+("홍길동","013-1234-5678", 2025, "060303-3", "COM"),
+("고길동","013-1111-1111", 2025, "060401-3", "COM"),
+("김길동","013-1111-2222", 2025, "060505-3", "COM"),
+("하니","013-1111-3333", 2025, "060606-4", "COM"),
+# 이름 나애리, 연락처 013-1111-4444, 입학연도 2025, 주민번호 060625-4, 디자인공학과
+# 이름 오영심, 연락처 013-1111-5555, 입학연도 2025, 주민번호 060717-4, 디자인공학과
+# 이름 오두리, 연락처 013-1111-6666, 입학연도 2025, 주민번호 060815-4, 디자인공학과
+# 이름 전지현, 연락처 013-1111-7777, 입학연도 2025, 주민번호 051003-4, 디자인공학과
+("나애리","013-1111-4444", 2025, "060625-4", "DEG"),
+("오영심","013-1111-5555", 2025, "060717-4", "DEG"),
+("오두리","013-1111-6666", 2025, "060815-4", "DEG"),
+("전지현","013-1111-7777", 2025, "051003-4", "DEG"),
+# 이름 김둘리, 연락처 013-1111-8888, 입학연도 2025, 주민번호 061224-3, 기계공학과
+# 이름 김군, 연락처 013-1111-9999, 입학연도 2025, 주민번호 070101-3, 기계공학과
+# 이름 박명수, 연락처 013-1111-0000, 입학연도 2025, 주민번호 060405-3, 기계공학과
+# 이름 유재석, 연락처 013-1111-1234, 입학연도 2025, 주민번호 061009-3, 기계공학과
+("김둘리","013-1111-8888", 2025, "061224-3", "MEC"),
+("김군","013-1111-9999", 2025, "070101-3", "MEC"),
+("박명수","013-1111-0000", 2025, "060405-3", "MEC"),
+("유재석","013-1111-1234", 2025, "061009-3", "MEC");
+
+# 최종 합격한 컴퓨터공학과 학생들을 학생으로 등록하는 쿼리 
+INSERT INTO STUDENT
+SELECT 
+	CONCAT(FP_YEAR, DE_NAME_NUM, LPAD(RANK() OVER(ORDER BY FP_NAME, FP_RES_NUM) , 3, "0")) AS ST_NUM,
+	FP_NAME, 
+    FP_RES_NUM, 
+    FP_PHONE, 
+    FP_YEAR ,
+    DE_NUM,
+    NULL
+FROM 
+	FINAL_PASS 
+JOIN
+	DEPARTMENT ON FP_DE_NUM = DE_NUM
+WHERE 
+	FP_DE_NUM = "COM" AND FP_YEAR = YEAR(NOW());
+# 최종 합격한 디자인공학과 학생들을 학생으로 등록하는 쿼리 
+INSERT INTO STUDENT
+SELECT 
+	CONCAT(FP_YEAR, DE_NAME_NUM, LPAD(RANK() OVER(ORDER BY FP_NAME, FP_RES_NUM) , 3, "0")) AS ST_NUM,
+	FP_NAME, 
+    FP_RES_NUM, 
+    FP_PHONE, 
+    FP_YEAR ,
+    DE_NUM,
+    NULL
+FROM 
+	FINAL_PASS 
+JOIN
+	DEPARTMENT ON FP_DE_NUM = DE_NUM
+WHERE 
+	DE_NAME = "디자인공학과" AND FP_YEAR = YEAR(NOW());
+# 최종 합격한 기계공학과 학생들을 학생으로 등록하는 쿼리 
+INSERT INTO STUDENT
+SELECT 
+	CONCAT(FP_YEAR, DE_NAME_NUM, LPAD(RANK() OVER(ORDER BY FP_NAME, FP_RES_NUM) , 3, "0")) AS ST_NUM,
+	FP_NAME, 
+    FP_RES_NUM, 
+    FP_PHONE, 
+    FP_YEAR ,
+    DE_NUM,
+    NULL
+FROM 
+	FINAL_PASS 
+JOIN
+	DEPARTMENT ON FP_DE_NUM = DE_NUM
+WHERE 
+	DE_NAME = "기계공학과" AND FP_YEAR = YEAR(NOW());
+
+
+
