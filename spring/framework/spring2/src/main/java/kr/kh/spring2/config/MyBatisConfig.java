1bda75cdc0a4b65fd58dd6e81955baf7069c4c83
st8324 <stajun@naver.com>
2025-04-11 11:10:42 +0900

    스프링2 : xml 없이 설정 완료

--- spring/framework/spring2/src/main/java/kr/kh/spring2/config/MyBatisConfig.java
+++ spring/framework/spring2/src/main/java/kr/kh/spring2/config/MyBatisConfig.java
@@ -5,23 +5,32 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @MapperScan(basePackages = "kr.kh.spring2.dao") // 다오 인터페이스 패키지 경로 설정
+@PropertySource("classpath:config.properties")
 public class MyBatisConfig {
 
+	@Value("${db.username}")
+	private String username;
+	
+	@Value("${db.password}")
+	private String password;
+	
     // MySQL 데이터 소스 설정
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/community?useSSL=false&serverTimezone=Asia/Seoul");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
