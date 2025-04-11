30bfea3f5574a70c96296f3bde7dddc04f06724b
st8324 <stajun@naver.com>
2025-04-11 09:50:35 +0900

    스프링2 : 프로젝트 생성, 타일즈 적용까지 xml 제거 완료

---
+++ spring/framework/spring2/src/main/java/kr/kh/spring2/config/WebMvcConfig.java
@@ -0,0 +1,45 @@
+package kr.kh.spring2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "kr.kh.spring2") 
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+    
+	// TilesViewResolver 설정
+    @Bean
+    public TilesViewResolver tilesViewResolver() {
+        TilesViewResolver tilesViewResolver = new TilesViewResolver();
+        tilesViewResolver.setOrder(1); // ViewResolver의 우선순위 설정
+        return tilesViewResolver;
+    }
+    // Tiles 설정
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions("/WEB-INF/spring/tiles-def.xml");
+        tilesConfigurer.setCheckRefresh(true); // 변경 사항을 자동으로 감지하여 갱신
+        return tilesConfigurer;
+    }
+}
