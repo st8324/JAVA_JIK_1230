8b720ff09934deca9b66924f649f2a7740e70ecd
st8324 <stajun@naver.com>
2025-04-24 14:51:11 +0900

    부트 : 댓글 목록 구현, 댓글 페이지네이션 구현 중

--- spring/boot/boot/src/main/java/kr/kh/boot/controller/CommentController.java
+++ spring/boot/boot/src/main/java/kr/kh/boot/controller/CommentController.java
@@ -1,12 +1,17 @@
 package kr.kh.boot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.kh.boot.service.CommentService;
+import kr.kh.boot.utils.*;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import kr.kh.boot.model.vo.*;
@@ -25,4 +30,13 @@ public class CommentController {
 		return commentService.insertComment(comment, customUser);
 	}
 	
+	@PostMapping("/list")
+	public String list(Model model, @RequestBody CommentCriteria cri) {
+		cri.setPerPageNum(3);
+		List<CommentVO> list = commentService.getCommentList(cri);
+		PageMaker pm = commentService.getPageMaker(cri);
+		model.addAttribute("list", list);
+		model.addAttribute("pm", pm);
+		return "post/comment";
+	}
 }
