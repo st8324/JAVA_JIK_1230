7289a505ff44ed94a94b35b9220883f00b93bc14
st8324 <stajun@naver.com>
2025-04-29 11:13:52 +0900

    쇼핑몰 : 자동 로그인 구현 완료

--- spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/config/SecurityConfig.java
+++ spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package kr.kh.shoppingmall.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +10,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import kr.kh.shoppingmall.service.MemberDetailService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	@Autowired
+	MemberDetailService memberDetailService;
+	
+	@Value("${spring.rememeber.me.key}")
+	String rememberMeKey;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http.authorizeHttpRequests(
@@ -25,6 +35,13 @@ public class SecurityConfig {
 					.permitAll()
 					.loginProcessingUrl("/loginPost")
 					.defaultSuccessUrl("/")
+			)
+			//자동 로그인 처리
+			.rememberMe(rm-> rm
+				.userDetailsService(memberDetailService)//자동 로그인할 때 사용할 userDetailService를 추가
+				.key(rememberMeKey)//키가 변경되면 기존 토큰이 무효처리
+				.rememberMeCookieName("LC")//쿠키 이름
+				.tokenValiditySeconds(60 * 60 * 24 * 7)//유지 기간 : 7일
 			)
 			.logout(logout -> 
 				logout
