973251eb019820a25a10c2502ba299a6f24b0854
st8324 <stajun@naver.com>
2025-04-29 15:37:19 +0900

    쇼핑몰 : 헤더 - 카테고리 리스트 추가

--- spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/MainController.java
+++ spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/MainController.java
@@ -1,14 +1,19 @@
 package kr.kh.shoppingmall.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.kh.shoppingmall.model.vo.CategoryVO;
 import kr.kh.shoppingmall.model.vo.MemberVO;
 import kr.kh.shoppingmall.service.MemberService;
+import kr.kh.shoppingmall.service.ProductService;
 
 
 
@@ -17,6 +22,8 @@ public class MainController {
 
 	@Autowired
 	MemberService memberService;
+	@Autowired
+	ProductService productService;
 	
 	@GetMapping("/")
 	public String main() {
@@ -45,5 +52,10 @@ public class MainController {
 	public String login() {
 		return "user/login";
 	}
-	
+	@GetMapping("/category")
+	public String category(Model model) {
+		List<CategoryVO> list = productService.getCategory();
+		model.addAttribute("list", list);
+		return "layout/header";
+	}
 }
